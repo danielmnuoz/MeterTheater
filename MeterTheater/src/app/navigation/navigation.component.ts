152b926549da5e6de98975a6b497cbf659bd3f06
danielmnuoz <dmnuozl@gmail.com>
2022-07-25 16:19:32 -0400

    changed header and details page

--- MeterTheater/src/app/navigation/navigation.component.ts
+++ MeterTheater/src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MeterTheaterDBService } from '../meter-theater-db.service';
 
 @Component({
   selector: 'app-navigation',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dbservice: MeterTheaterDBService
+  ) { }
 
   ngOnInit(): void {
   }
