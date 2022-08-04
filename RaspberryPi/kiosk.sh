9b3778edd82565a51efa69cec69a7396e5c9b44b
knguyen382 <96245593+knguyen382@users.noreply.github.com>
2022-08-04 11:43:06 -0400

    Create kiosk.sh

---
+++ RaspberryPi/kiosk.sh
@@ -0,0 +1,16 @@
+#located at home/theaterbusser/kiosk.sh
+#!/bin/bash
+#stops the Pi from blanking out 
+xset s noblank
+xset s off
+xset -dpms
+
+#runs unclutter that hides the mouse if idling for more than 5 sec
+unclutter -idle 5 -root &
+
+#clear warning bars
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/theaterbusser/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/theaterbusser/.config/chromium/Default/Preferences
+
+#launches metertheater in chromium in Kiosk
+/usr/bin/chromium-browser  --kiosk https://metertheater &
